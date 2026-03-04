18ab36bebe4f2c7d354bda7306eee09080e9cea5
Valentin <va.lucarini00@gmail.com>
2026-03-04 12:48:16 -0300

    chore: update github pages site url with correct username

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://valentin.github.io',
+    site: 'https://valentnluc.github.io',
     base: '/portfolio',
     image: {
         domains: ['images.unsplash.com', 'plus.unsplash.com'],
